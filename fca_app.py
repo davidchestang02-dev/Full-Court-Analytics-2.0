40a1fca6490ad637aa0dbd310d8b5efc4e1ffe53
davidchestang02-dev <david.chestang02@gmail.com>
2026-03-03 17:05:12 -0500

    Update fca_app.py

--- fca_app.py
+++ fca_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Full Court Analytics 2.0", layout="wide")
+
+st.title("Full Court Analytics 2.0")
+st.write("Use the left sidebar to navigate.")
